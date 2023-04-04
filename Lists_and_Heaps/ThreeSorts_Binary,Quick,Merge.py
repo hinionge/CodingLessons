268ac51553d133f30ba2b444261a51e993ce9289
hinionge <hinionge@outlook.com>
2023-04-04 14:59:58 +0100

    Binary insertion sort added, but I don't understand it

--- Lists_and_Heaps/ThreeSorts_Binary,Quick,Merge.py
+++ Lists_and_Heaps/ThreeSorts_Binary,Quick,Merge.py
@@ -3,6 +3,28 @@
 
 import random
 
+
+def binary_search(list, value, start, end):
+
+    if start == end:
+        if list[start] > value:
+            return start
+        else:
+            return start + 1
+
+    if start > end:
+        return start
+
+    mid = (start + end) // 2
+
+    if list[mid] < value:
+        return binary_search(list, value, mid+1, end)
+    elif list[mid] > value:
+        return binary_search(list, value, start, mid-1)
+    else:
+        return mid
+
+
 def insertion_sort(list):
     for j in range(1,len(list)):
         key = list[j]
@@ -15,9 +37,15 @@ def insertion_sort(list):
 
     return list
 
-                            # Placeholder for binary sort
-def binary_sort(list):
-    pass
+def binary_insertion_sort(list):
+    for i in range(1,len(list)):
+        key = list[i]
+        j = binary_search(list, key, 0, i-1)
+                            # This line here I don't follow
+        list = list[:j] + [key] + list[j:i] + list[i+1:]
+    return list
+
+
 
                             # Placeholder for quick sort
 def quick_sort(list):
@@ -56,4 +84,8 @@ if __name__ == "__main__":
                             # Another random list
     mylist = generate_random_list(length,upper,lower)
     print("\nUnsorted list:")
+    print(mylist)
+
+    mylist = binary_insertion_sort(mylist)
+    print("Binary insertion sort:")
     print(mylist)
