3f6d1c523cffe4d180dbf7ee8a54be9609cdf91d
hinionge <117522318+hinionge@users.noreply.github.com>
2023-04-05 15:43:53 +0100

    Attempting to code QuickSort

--- Lists_and_Heaps/ThreeSorts_Binary,Quick,Merge.py
+++ Lists_and_Heaps/ThreeSorts_Binary,Quick,Merge.py
@@ -47,9 +47,51 @@ def binary_insertion_sort(list):
 
 
 
-                            # Placeholder for quick sort
+
+                            # 5.4.2023 third or fourth attempt at Quick Sort
+                            # .... first step SEEMS to work? though it doesn't always catch every number, for some reason
+                            #
+                            # While trying to code it, using inbuilt pop() and insert() rather than 'manually' handling list items, for simplicity
+
+
 def quick_sort(list):
-    pass
+                            # Traps for when recursion has gone far enough and the list is 1 or 2 elements
+    if len(list) == 0:
+        return list
+    if len(list) == 1:
+        return list
+    elif len(list) == 2:
+        if list[0] > list[1]:
+            temp = list[0]
+            list[0] = list[1]
+            list[1] = temp
+            return list
+    else:
+        #peg = random.randint(1,len(list)-2)
+                            # Stick with peg at index 5 while trying to make it work
+        peg = 5
+        print("Peg at index " + str(peg) + ": L/R sort value is " + str(list[peg]))
+
+        height = list[peg]
+
+
+                            # Loop to find numbers to the left of peg which are larger: if found, adds to end of list and removes in position
+        j = 0
+        while peg > 1 and j < peg+1:
+            if list[j] > height:
+                list.append(list[j])
+                list.pop(j)
+                peg -= 1
+                j -= 1
+            j += 1
+
+                            # Loop to find numbers on right of peg which are smaller, and shove them to the start of the list
+        for j in range(peg,len(list)-1):
+            if list[j] < height:
+                list.insert(0,list[j])
+                list.pop(j+1)
+
+        return list
 
                             # Placeholder for merge sort
 def merge_sort(list):
@@ -68,24 +110,32 @@ def generate_random_list(length,intmin,intmax):
 
 
 if __name__ == "__main__":
-    length = 43
+    length = 15
     upper = 1
     lower = 100
-                            # Generate a list of 100 random numbers between 1 and 100 inclusive
-    mylist = generate_random_list(length,upper,lower)
-    print("\nUnsorted list:")
-    print(mylist)
-
-                            # Demonstrate insertion sort
-    mylist = insertion_sort(mylist)
-    print("Insertion sort:")
-    print(mylist)
+    #                         # Generate a list of 100 random numbers between 1 and 100 inclusive
+    # mylist = generate_random_list(length,upper,lower)
+    # print("\nUnsorted list:")
+    # print(mylist)
+    #
+    #                         # Demonstrate insertion sort
+    # mylist = insertion_sort(mylist)
+    # print("Insertion sort:")
+    # print(mylist)
+    #
+    #                         # Another random list
+    # mylist = generate_random_list(length,upper,lower)
+    # print("\nUnsorted list:")
+    # print(mylist)
+    #
+    # mylist = binary_insertion_sort(mylist)
+    # print("Binary insertion sort:")
+    # print(mylist)
 
                             # Another random list
     mylist = generate_random_list(length,upper,lower)
     print("\nUnsorted list:")
     print(mylist)
 
-    mylist = binary_insertion_sort(mylist)
-    print("Binary insertion sort:")
+    mylist = quick_sort(mylist)
     print(mylist)
