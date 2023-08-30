90d1b419f7ecaf1608ac63cae9ec6ee210d3b748
hinionge <117522318+hinionge@users.noreply.github.com>
2023-08-30 10:32:07 +0100

    Started a module hinimath.py to store often-used maths functions

--- Project_Euler/ProjectEuler010_SummationofPrimes.py
+++ Project_Euler/ProjectEuler010_SummationofPrimes.py
@@ -7,37 +7,10 @@
 
 # 24.8.2023 -- this is more than ten times faster now that I've introduced a sieve into the prime check
 
-import math
 import time
-
-def prime(n):
-                                # Special cases:
-    if n == 1 or n == 0:
-        return False
-    if n == 2:
-        return True
-
-    max = (int(math.ceil(math.sqrt(n))))
-
-    sieve = [0]
-
-    for s in range(max):
-        sieve.append(1)
-
-    d = 3
-                                # Count up to sqrt(n)
-    while d <= max:
-                                # Only check a new divisor if it has not already been eliminated
-        if sieve[d] == 1:
-            if n % d == 0:
-                return False
-                                # If a number d does not divide n, then eliminate all multiples of d from the search
-        for j in range(d,len(sieve),d):
-            sieve[j] = 0
-        d += 2
-
-    return True
-
+from hinimath import prime
+# ed. on 30.8: it works! haha (but, only if hinimath.py is in the exact same directory. that's fine, for now)
+# and you can't import the whole module at once, only individual ones from within it -- why?
 
 def sum_primes_below(max):
     n = 2
@@ -57,7 +30,7 @@ def sum_primes_below(max):
     return primesum
 
 if __name__ == "__main__":
-    below = 2000000
+    below = 200000
     begin_clock = time.time()
     print ("The sum of primes below " + str(below) + " is " + str(sum_primes_below(below)))
     end_clock = time.time()-begin_clock
