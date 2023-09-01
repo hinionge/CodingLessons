75a8cefecdd53978ca6a28f6d8f9c9f12cf2bd1e
hinionge <117522318+hinionge@users.noreply.github.com>
2023-09-01 14:06:48 +0100

    Updated PE3

--- Project_Euler/ProjectEuler003_LargestPrimeFactor.py
+++ Project_Euler/ProjectEuler003_LargestPrimeFactor.py
@@ -1,9 +1,13 @@
 
 # 8.3.2023 Revisiting some Project Eulers I lost in December
 
-# this doesn't work yet
+# 1.9.2023
+# just updated to use new prime check
+# (so it presumably DID work before, it was just so incredibly slow that i never let it finish
+# -- it now completes in c. 16 seconds)
 
 import time
+from hinimath import prime
 
                             # Method returns True if m divides n with modulo zero
 def does_it_divide(m,n):
@@ -13,33 +17,19 @@ def does_it_divide(m,n):
     return factor
 
                             # Primality test
-def is_it_prime(n):
-    prime = True
-    half = int(n/2)
-                            # (test counts down from half of n, one integer at a time, asks if it divides:
-    for c in range(half,1,-1):
-                            # (if it ever does, n is not prime)
-        if does_it_divide(c,n) == True:
-            prime = False
-                            # There is a special case for excluding 1, which would otherwise return as prime
-    if n == 1:
-        prime = False
-    return prime
-
 def find_largest_prime_factor(n):
     ans = 1
     found = False
     div = 2
     while found == False:
         if does_it_divide(div,n):
-            if is_it_prime(n/div):
+            if prime(n/div):
                 ans = n/div
                 found = True
         div = div + 1
     return int(ans)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     inputn = input("Test number: ")
     inputn = int(inputn)
